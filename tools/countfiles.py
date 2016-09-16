ea0a93ce219f53631381206b10435e7a9a4c96cf
Wolfram Eifler <wolfram@lizardfs.com>
2016-09-16 16:43:07 +0200

    doc: Reworked statistics for authtags-by-author

--- tools/countfiles.py
+++ tools/countfiles.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# todo: add by-author functionality
+
 import sys, os, re
 
 filepat = re.compile("^.*\.rst$")	# pattern for rst-files
@@ -15,11 +17,20 @@ def addto_list_in_hash(h, key, elem):
   else: 
     h[key] = [elem]
 
+def addto_hash_in_hash(h, key, subkey, val):
+  """adds elem to hash in h[key], creates hash if necessary
+  resulting structure is hash[key] -> hash[subkey] -> list
+  """
+  if not h.has_key(key):
+    h[key] = {}
+  addto_list_in_hash(h[key], subkey, val)
+
 class AuthStati:
   def __init__(self, topdir):
     self.topdir = topdir
     self.bypath = {}		# filepath -> auth-stati
     self.bystat = {}		# auth-status -> filepathes
+    self.byauthor = {}		# auth-status -> filepathes
 
   def remember(self, fullpath, auth_status, author):
     if not fullpath.startswith(self.topdir):
@@ -28,9 +39,8 @@ class AuthStati:
       sys.exit(1)
     path = fullpath[len(self.topdir)+1:]	# strip common prefix
     addto_list_in_hash(self.bypath, path, auth_status + '/' + author)
-    astat = auth_status		# tag status with '*' if there is no author
-    if (author == 'none') or (author == ''): astat = '*' + astat
-    addto_list_in_hash(self.bystat, astat, path)
+    addto_list_in_hash(self.bystat, auth_status, path)
+    addto_hash_in_hash(self.byauthor, author, auth_status, path)
 
   def process_rstfile(self, pathname, filename):
     """scan rstfile for auth-stat pattern and trigger "remember them" """
@@ -55,19 +65,26 @@ class AuthStati:
 
   def __str__(self):
     result = ""
+#    tags_total = 0
+#    result += "\nNumber of Auth-Status tags per file:\n"
+#    for key, val in self.count_hash_valslen(self.bypath).items():
+#      result += "  %i\t%s\n" % (val, key)
+#      tags_total += val
+#    result += "total: %i\n" % tags_total
     tags_total = 0
-    result += "\nNumber of Auth-Status tags per file:\n"
-    for key, val in self.count_hash_valslen(self.bypath).items():
-      result += "  %i\t%s\n" % (val, key)
-      tags_total += val
-    result += "total: %i\n" % tags_total
-    tags_total = 0
-    result += "\nNumber of Auth-Stati by Status-type, "
-    result += "types tagged with '*' have no author\n"
+    result += "\nNumber of Auth-Stati by Status-type\n"
     for key, val in self.count_hash_valslen(self.bystat).items():
       result += "  %i\t%s\n" % (val, key)
       tags_total += val
-    result += "total: %i\n" % tags_total
+    result += "  total: %i\n" % tags_total
+    result += "Tags listed by author:\n"
+    for key, subhash in self.byauthor.items():
+      result += "Author=%s\n" % key
+      tags_total = 0
+      for key, val in self.count_hash_valslen(subhash).items():
+        result += "  %i\t%s\n" % (val, key)
+        tags_total += val
+      result += "  total: %i\n" % tags_total
     return(result)
 
 # store to hold information about files/auth-stati
