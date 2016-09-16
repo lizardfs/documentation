3b8520411259801f62844ad3e818e5b251755282
Wolfram Eifler <wolfram@lizardfs.com>
2016-09-17 00:52:40 +0200

    doc: Added todo-counting to statistics-tool

--- tools/countfiles.py
+++ tools/countfiles.py
@@ -5,8 +5,10 @@
 import sys, os, re
 
 filepat = re.compile("^.*\.rst$")	# pattern for rst-files
-# pattern for authoring-tag in file
-linepat = re.compile("^\.\. auth-status-(.*)/(.*)$")
+# pattern for authoring-tags in file
+authpat = re.compile("^\.\. auth-status-(.*)/(.*)$")
+# pattern for todo-tag in file
+todopat = re.compile("^\.\. todo::(.*)$")
 toolspath =  os.path.dirname(os.path.abspath(sys.argv[0]))
 topdir =  os.path.join( os.path.split(toolspath)[0], 'source')
 
@@ -28,33 +30,45 @@ def addto_hash_in_hash(h, key, subkey, val):
 class AuthStati:
   def __init__(self, topdir):
     self.topdir = topdir
-    self.bypath = {}		# filepath -> auth-stati
-    self.bystat = {}		# auth-status -> filepathes
-    self.byauthor = {}		# auth-status -> filepathes
+    self.bypath = {}		# hash[filepath] -> auth-stati
+    self.bystat = {}		# hash[auth-status] -> filepathes
+    self.byauthor = {}		# hash[author] -> hash[auth-status] -> filepathes
+    self.todos_bypath = {}      # hash[filepath] -> count of todos
 
-  def remember(self, fullpath, auth_status, author):
+  def strip_topdir(self, fullpath):
+    """strips topdir from beginning of fullpath, exits program
+    if fullpath does not start with topdir"""
     if not fullpath.startswith(self.topdir):
       # should never go here
-      print "ERROR: fullpath to remeber is not under topdir, exiting."
+      sys.stderr.write(
+        "ERROR: fullpath to remeber is not under topdir, exiting.\n")
       sys.exit(1)
-    path = fullpath[len(self.topdir)+1:]	# strip common prefix
+    return(fullpath[len(self.topdir)+1:])	# strip common prefix
+    
+  def remember_authstat(self, fullpath, auth_status, author):
+    path = self.strip_topdir(fullpath)
     addto_list_in_hash(self.bypath, path, auth_status + '/' + author)
     addto_list_in_hash(self.bystat, auth_status, path)
     addto_hash_in_hash(self.byauthor, author, auth_status, path)
 
+  def add_todo(self, fullpath, todostr):
+    """add 1 to number of todos found for file fullpath"""
+    path = self.strip_topdir(fullpath)
+    addto_list_in_hash(self.todos_bypath, path, todostr)
+    
   def process_rstfile(self, pathname, filename):
     """scan rstfile for auth-stat pattern and trigger "remember them" """
     fullpath = os.path.join(pathname, filename)
     input = open(fullpath, 'rb')
-    l = []
-    for line in input: 
-      m = linepat.match(line)
+    for line in input:  # find and remember authoring-tags
+      m = authpat.match(line)
       if m:
-        l.append(m.groups())
+        (auth_status, author) = m.groups()
+        self.remember_authstat(fullpath, auth_status, author)
+      m = todopat.match(line)
+      if m:
+        self.add_todo(fullpath, line.strip())
     input.close()
-    if l:
-      for elem in l: 
-        self.remember(fullpath, elem[0], elem[1])
 
   def count_hash_valslen(self, h):
     """counts the length of values in h for each key"""
@@ -85,6 +99,12 @@ class AuthStati:
         result += "  %i\t%s\n" % (val, key)
         tags_total += val
       result += "  total: %i\n" % tags_total
+    tags_total = 0
+    result += "Number of todo-tags per file\n"
+    for key, val in self.count_hash_valslen(self.todos_bypath).items():
+      result += "  %i\t%s\n" % (val, key)
+      tags_total += val
+    result += "  total: %i\n" % tags_total   
     return(result)
 
 # store to hold information about files/auth-stati
