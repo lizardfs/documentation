e7845d306b4db021294bcb8dc5e60cf18964b647
Michal Bielicki <m.bielicki@lizardfs.com>
2016-09-19 15:16:31 +0200

    doc: updated man generating script to convert utf8 to latin1 format

--- source/man/create_man_pages.sh
+++ source/man/create_man_pages.sh
@@ -1,5 +1,5 @@
 # Script to convert all files from rst to manpages using pandoc
 for i in *.rst
 do
-/usr/local/bin/pandoc -s -f rst -t man $i > "${i/%.rst/}"
+/usr/local/bin/pandoc -s -f rst -t man $i | iconv -f utf-8 -tlatin1 > "${i/%.rst/}"
 done
